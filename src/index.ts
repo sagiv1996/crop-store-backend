c82fcb0d7f66ef25f3dc5921e3a3b51889e644a6
sagiv bonali <sagivb13@gmail.com>
2023-07-04 17:02:48 +0300

    return distance from user location

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+////////////////////////
+import { getDistance } from "geolib";
+
 export default {
   /**
    * An asynchronous register function that runs before
@@ -5,8 +8,62 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.service("plugin::graphql.extension");
 
+    const extension = ({ nexus }) => ({
+      types: [
+        nexus.extendType({
+          type: "Store",
+          definition: (t) => {
+            t.float("distance"); // Add the 'distance' field to the Store type
+          },
+        }),
+      ],
+    });
+
+    strapi.plugin("graphql").service("extension").use(extension);
+
+    extensionService.use(({ strapi }) => ({}));
+
+    extensionService.use(({ strapi }) => ({
+      typeDefs: `
+      type Query {
+          stores(lat: Float!, lng: Float!): StoreEntityResponseCollection
+        }
+        `,
+      resolvers: {
+        Query: {
+          stores: {
+            resolve: async (parent, args, context) => {
+              const { lat, lng } = args;
+              const { toEntityResponseCollection } = strapi
+                .plugin("graphql")
+                .service("format").returnTypes;
+
+              const data = await strapi.services["api::store.store"].find();
+
+              for (const result of data.results) {
+                const distance = getDistance(
+                  { lat, lng },
+                  {
+                    lat: Number(result.location.lat),
+                    lng: Number(result.location.lng),
+                  }
+                );
+                result.distance = distance;
+              }
+              data.results.sort((a, b) => a.distance - b.distance);
+              const result = toEntityResponseCollection(data.results);
+
+              return result;
+            },
+          },
+        },
+      },
+    }));
+    extensionService.use(extensionService);
+  },
   /**
    * An asynchronous bootstrap function that runs before
    * your application gets started.
